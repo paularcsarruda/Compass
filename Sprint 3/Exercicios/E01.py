28845fc05936320c61abc83d8e713f17951a8991
Paula Arruda <122739036+paularcsarruda@users.noreply.github.com>
2024-01-17 22:45:46 -0300

    Update E01.py

--- Sprint 3/Exercicios/E01.py
+++ Sprint 3/Exercicios/E01.py
@@ -3,6 +3,6 @@ from datetime import datetime
 nome = 'Paula'
 idade_atual = 37
 ano_atual = datetime.now().year
-ano_100_anos = ano_atual + (100 - idade_atual)
+centenario = ano_atual + (100 - idade_atual)
 
-print(f"{ano_100_anos}")
+print(f"{centenario}")
