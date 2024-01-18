5884a1eecb8d414d18b0528329feea75695bad64
Paula Arruda <122739036+paularcsarruda@users.noreply.github.com>
2024-01-18 14:31:52 -0300

    Update etapa-4.py

--- Sprint 3/Tarefa/etapa-4.py
+++ Sprint 3/Tarefa/etapa-4.py
@@ -1,23 +1,19 @@
-from collections import Counter
+with open('actors.csv', 'r', encoding='utf-8') as file:
+    linhas = file.readlines()
 
-count_films = Counter()
+cabecalho = linhas[0].strip().split(',')
+num_filmes = {}
 
-with open("actors.csv") as arquivo:
-    dados = arquivo.read().replace('"Robert Downey, Jr."', "Robert Downey Jr.").split('\n')
-    with open('etapa-4.txt', 'w') as txt:
+for linha in linhas[1:]:
+    filme = linha.strip().split(',')[-2]
 
-        freq = Counter()
+    num_filmes[filme] = num_filmes.get(filme, 0) + 1
 
-        for a in dados[1:]:
-            campo1 = a.strip().split(',')[4]
+num_filmes_sorted = sorted(num_filmes.items(), key=lambda x: (x[1], x[0]), reverse=True)
 
-            if campo1:
-                freq[campo1] += 1
+with open('etapa-4.txt', 'w', encoding='utf-8') as file:
+    sequencia = 1
 
-        if freq:
-            result = sorted(freq.items(), key=lambda x: (-x[1], x[0]))
-            txt.write("Lista de todos os filmes e suas frequências no dataset:\n\n")
-            for sequencia, (nome_filme, quantidade) in enumerate(result, start=1):
-                txt.write(f'{sequencia} - O filme "{nome_filme}" aparece {quantidade} vez(es) no dataset.\n')
-        else:
-            txt.write('Nenhum filme encontrado no dataset.\n')
+    for filme, num_filmes in num_filmes_sorted:
+        file.write(f'{sequencia} - O filme {filme} aparece {num_filmes} vez(es) no dataset.\n')
+        sequencia += 1
