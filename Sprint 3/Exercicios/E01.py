d2021cf009b0ac5e03957c70ca22e1fa40e5dc2f
Paula Arruda <122739036+paularcsarruda@users.noreply.github.com>
2024-01-19 10:56:56 -0300

    Update E01.py

--- Sprint 3/Exercicios/E01.py
+++ Sprint 3/Exercicios/E01.py
@@ -1,7 +1,20 @@
+# primeira forma (correspondea resposta informada na Udemy)
+
 from datetime import datetime
 
 nome = 'Paula'
 idade_atual = 37
+ano_atual = datetime.now().year
+centenario = ano_atual + (100 - idade_atual)
+
+print(f"{centenario}")
+
+# segunda forma
+
+from datetime import datetime
+
+nome = input('Informe o seu nome: ')
+idade_atual = int(input('Informe a sua idade: '))
 ano_atual = datetime.now().year
 centenario = ano_atual + (100 - idade_atual)
 
