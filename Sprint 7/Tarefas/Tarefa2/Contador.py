eecc0a71fd45e16b4f0da19c6fd7ff46233f0710
paularcsarruda <paularcsarruda@gmail.com>
2024-03-12 15:35:32 -0300

    Sprint 7

--- Sprint 7/Tarefas/Tarefa2/Contador.py
+++ Sprint 7/Tarefas/Tarefa2/Contador.py
@@ -4,14 +4,18 @@ import re
 sc = SparkContext.getOrCreate()
 # Arquivo README.md
 rdd = sc. textFile("/home/jovyan/README.md")
+
 # Removendo as tags HTML
 rdd_words = rdd.flatMap(lambda linha: re.findall(r'\b|w+\b', linha))
+
 # Filtrando as palavras vazias
 rdd_words = rdd_words.filter(lambda palavra: len(palavra) > 0)
+
 # Contando as ocorrências de cada palavra
 contagem = rdd_words.map(lambda palavra: (palavra. lower(), 1)) \
                     .reduceByKey(lambda a, b: a + b) \
                     .sortByKey()
+
 total_palavras = rdd_words.count()
 resultado = contagem.collect ()
 
