5e56f28527a49d0ad0a366a83d5a63868528e76b
Paula Arruda <122739036+paularcsarruda@users.noreply.github.com>
2024-03-14 18:02:36 -0300

    Update Tarefa1-4.py

--- Sprint 7/Tarefas/Tarefa1/Tarefa1-4.py
+++ Sprint 7/Tarefas/Tarefa1/Tarefa1-4.py
@@ -5,7 +5,7 @@ Apresente o nome do(s) filme(s) mais frequente(s) e sua respectiva frequência.
 import pandas as pd
 
 # Carregando o arquivo CSV
-df = pd.read_csv('/Users/paulaarruda/Desktop/Compass/Sprint 7/Tarefas/actors.csv')
+df = pd.read_csv('/Users/paulaarruda/Compass/Sprint 7/Tarefas/Tarefa1/actors.csv')
 
 dfCount = df["#1 Movie"].value_counts().reset_index()
 dfCount.columns = ["Filmes", "Frequência"]
